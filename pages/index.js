b911bc69758527ad7f33cee7f947ab351e517304
Lucas Gonçalves Leite <115735167+luc4sleite@users.noreply.github.com>
2025-02-16 15:07:39 +0000

    modificação da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Gabrielli, é tudo por você, se você gosta de mim, da uma risadinha!</h1>
+    return <h1>Gabrielli, é tudo por você, se você gosta de mim, da uma risadinha! 😂</h1>
 }
 
 export default Home;
